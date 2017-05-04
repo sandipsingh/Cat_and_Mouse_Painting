425e53071ee866fff077d1170a33c0bfd2a1047d
Sandip <sandip@avigma.com>
2017-05-04 12:41:47 +0530

    initial commit

---
+++ app/build/generated/source/buildConfig/debug/apps4kids/painting/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package apps4kids.painting;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "catmouse.painting.drawing";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 11;
+  public static final String VERSION_NAME = "1.10";
+  // Fields from build type: debug
+  public static final boolean DEBUG_MODE = true;
+}
